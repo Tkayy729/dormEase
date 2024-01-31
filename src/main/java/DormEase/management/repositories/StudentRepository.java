25ffd8c43b9718d13888dc403f468bf33507fed1
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2024-01-31 13:52:50 +0100

    added a student model

---
+++ src/main/java/DormEase/management/repositories/StudentRepository.java
@@ -0,0 +1,10 @@
+package DormEase.management.repositories;
+
+import DormEase.management.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Optional<Student> findByEmail(String email);
+}
