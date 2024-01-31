d4d455de99f13619609ef08f642d4a66a3ac1a9c
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2024-01-31 13:52:00 +0100

    controller to handle all student requests and response

---
+++ src/main/java/DormEase/management/controllers/StudentController.java
@@ -0,0 +1,9 @@
+package DormEase.management.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/v1/auth/")
+public class StudentController {
+}
